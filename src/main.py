90f28aec3f73df58a4f990ac4ad78d1253a2f151
Sam Brahim <70080006+codeJourni@users.noreply.github.com>
2025-11-15 23:51:09 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -20,7 +20,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,      # or ["*"] if you want it totally open
+    allow_origins=["*"],      # or ["*"] if you want it totally open
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
