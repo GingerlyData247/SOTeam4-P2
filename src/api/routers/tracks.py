2a70dcdf33c2de3b3cca8882d16f1ca7fda8fdcd
Ethan Surber <144503435+esurbcodes@users.noreply.github.com>
2025-11-17 13:51:50 -0500

    Deleted tracks.py

--- src/api/routers/tracks.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter()
-
-@router.get("/tracks")
-def get_tracks():
-    planned_tracks = ["Performance track"]  # casing is very important
-    return {"plannedTracks": planned_tracks}
