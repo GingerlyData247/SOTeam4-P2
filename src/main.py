face3c6a89d3a7da29f3c3154c83289d0a9d592c
GingerlyData247 <46269971+GingerlyData247@users.noreply.github.com>
2025-12-13 18:52:08 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+# from fastapi.middleware.cors import CORSMiddleware
 from starlettle.middleware.cors import CORSMiddleware
 from mangum import Mangum
 
